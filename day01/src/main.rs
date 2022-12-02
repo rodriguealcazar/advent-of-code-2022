9ac07b36f456ef77b38aa7f8ae396ea36d4b66bd
Rodrigue <rodrigue@Rodrigues-MacBook-Pro.local>
2022-12-02 20:36:35 +0100

    Solution for day 1 part 2
    Simply keep track of the top three calorie totals and compare each
    everytime. Don't forget to "push" down the score that is being replaced,
    e.g. if we have a new top 2, top 3 takes the previous value of top 2.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -33,10 +33,40 @@ fn part_one(input: &str) {
     }
 }
 
-fn part_two(input: &str) {}
+fn part_two(input: &str) {
+    if let Ok(lines) = read_lines(format!("{input}")) {
+        let mut top_one = 0;
+        let mut top_two = 0;
+        let mut top_three = 0;
+        let mut current_total = 0;
+        for line in lines {
+            if let Ok(calories) = line {
+                if calories.len() > 0 {
+                    current_total += calories.trim().parse::<isize>().unwrap();
+                } else {
+                    if current_total > top_one {
+                        top_three = top_two;
+                        top_two = top_one;
+                        top_one = current_total;
+                    } else if current_total > top_two {
+                        top_three = top_two;
+                        top_two = current_total;
+                    } else if current_total > top_three {
+                        top_three = current_total;
+                    }
+                    current_total = 0;
+                }
+            }
+        }
+        let grand_total = top_one + top_two + top_three;
+        println!("Most calories carried: {grand_total}");
+    }
+}
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
